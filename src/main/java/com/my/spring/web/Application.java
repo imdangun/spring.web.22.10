def5ba7be6f12d35c8fdc2f1933cd3b3c2da9d32
601-00 <601-00@DESKTOP-BJFHA7A>
2022-10-06 11:40:41 +0900

    a

--- src/main/java/com/my/spring/web/Application.java
+++ src/main/java/com/my/spring/web/Application.java
@@ -1,8 +1,12 @@
 package com.my.spring.web;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.my.hr.Application;
 
 @SpringBootApplication
 @MapperScan("com.my.spring.web.**.map")
@@ -10,5 +14,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		/*
+		SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.setBannerMode(Banner.Mode.OFF);     // 배너 감추기
+        application.run(args);
+        */
 	}
 }
